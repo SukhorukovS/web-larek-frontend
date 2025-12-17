ca42a0c3b735517824396a71923db8712cc79ec0
Sergey Sukhorukov <Se.Sukhorukov@x5.ru>
2025-12-17 21:51:23 +0400

    Add types

---
+++ src/types/index.ts
@@ -0,0 +1,34 @@
+export interface Product {
+  id: string;
+  title: string;
+  description: string;
+  image: string;
+  category: string;
+  price: number | null;
+}
+
+export interface ProductList {
+  total: number;
+  items: Product[];
+}
+
+export interface UserData {
+  email: string;
+  phone: string;
+  address: string;
+}
+
+export interface Order extends UserData {
+  payment: "online" | "offline";
+  total: number;
+  items: Pick<Product, 'id'>[]
+}
+
+export interface ApiError {
+  error: string;
+}
+
+export interface ApiOrder {
+  id: string;
+  total: number;
+}
